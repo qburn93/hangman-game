89c089876dbbb74a38b2c7d42a6b03672e9bed9f
Bogdan933 <bogdan.gugu@hotmail.com>
2022-10-10 06:16:29 +0000

    Declared all global variable and all functions

--- run.py
+++ run.py
@@ -1,3 +1,34 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Variable for correctly guessed letters(array)
+# Variable for incorrectly guessed letters(array)
+# Variable for randomly chosen word(string)
+# Variable for lives left(integer)
+# Variable for game over(boolian)
+
+    def random_word():
+    """Returns a random selected word from our list of acceptable words"""
+    pass
+
+
+def draw_word():
+    """Dashed out letters that are not guessed yet"""
+    pass
+
+
+def draw_hangman():
+    """Hangman printed based on number of guesses left"""
+    pass
+
+
+def get_valid_letter():
+    """Makes sure the number of letters typed is different each time and is typed only once"""
+    pass
+
+
+def guess_letter():
+    """Checks if the one letter guessed is correct or wrong, and updates global variables based on the result"""
+    pass
+ 
+
+ def main():
+    """Entry point of application, calls all other methods in a loop"""
+    pass
