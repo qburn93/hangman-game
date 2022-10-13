02d1ec014fadb18f690e0d4f6271fce81c3acc5b
Bogdan933 <bogdan.gugu@hotmail.com>
2022-10-13 08:48:56 +0000

    Re structuring looping function

--- run.py
+++ run.py
@@ -16,3 +16,14 @@ def main():
     display = '_' * length
     already_guessed = []
     play_game = ""
+
+def play_loop():
+    global play_game
+    play_game = input("Do You want to play again? y = yes, n = no \n")
+    while play_game not in ["y", "n","Y","N"]:
+        play_game = input("Do You want to play again? y = yes, n = no \n")
+    if play_game == "y":
+        main()
+    elif play_game == "n":
+        print("Thanks For Playing! We expect you back again!")
+        exit()
